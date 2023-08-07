d44dd5ae85376cb7f10051c375507770b3f30a3c
Olga Korol <oggi.fine@gmail.com>
2023-08-07 19:49:06 +0300

    double test added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,7 +6,7 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test   //1
 
-    public void shouldRequiredAmount() {
+    public void shouldRequiredAmountJunit4() {
 
         CashbackHackService hackService = new CashbackHackService();
         int amount = 900;
@@ -14,22 +14,22 @@ public class CashbackHackServiceTest {
         int expected = 100;
         int actual = hackService.remain(amount);
 
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
     @org.junit.Test  //2 граничное значение 999
-    public void shouldRequiredAmount1() {
+    public void shouldRequiredAmount1Junit4() {
 
         CashbackHackService hackService = new CashbackHackService();
         int amount = 999;
         int expected = 1;
         int actual = hackService.remain(amount);
 
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
     @org.junit.Test //3 граничное значение 1000
-    public void shouldRequiredAmount0() {
+    public void shouldRequiredAmount0Junit4() {
 
         CashbackHackService hackService = new CashbackHackService();
         int amount = 1000;
@@ -37,11 +37,11 @@ public class CashbackHackServiceTest {
         int expected = 0;
         int actual = hackService.remain(amount);
 
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
     }
 
     @org.junit.Test //4 граничное значение 1001
-    public void shouldRequiredAmount999() {
+    public void shouldRequiredAmount999Junit4() {
 
         CashbackHackService hackService = new CashbackHackService();
         int amount = 1_001;
@@ -49,6 +49,54 @@ public class CashbackHackServiceTest {
         int expected = 999;
         int actual = hackService.remain(amount);
 
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test   //1
+
+    public void shouldRequiredAmountApi() {
+
+        CashbackHackService hackService = new CashbackHackService();
+        int amount = 900;
+
+        int expected = 100;
+        int actual = hackService.remain(amount);
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test  //2 граничное значение 999
+    public void shouldRequiredAmount1Api() {
+
+        CashbackHackService hackService = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = hackService.remain(amount);
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test //3 граничное значение 1000
+    public void shouldRequiredAmount0Api() {
+
+        CashbackHackService hackService = new CashbackHackService();
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = hackService.remain(amount);
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test //4 граничное значение 1001
+    public void shouldRequiredAmount999Api() {
+
+        CashbackHackService hackService = new CashbackHackService();
+        int amount = 1_001;
+
+        int expected = 999;
+        int actual = hackService.remain(amount);
+
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 }
